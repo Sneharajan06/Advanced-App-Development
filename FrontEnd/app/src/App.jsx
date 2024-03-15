c310f2c763a168726494aae8254ae419ac56b978
Sneharajans <727821tuit141@skct.edu.in>
2024-03-15 16:27:42 +0530

    Day 2 login,register,navbar

--- FrontEnd/app/src/App.jsx
+++ FrontEnd/app/src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
+import Login from './components/login'; 
+import Register from './components/register';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-      sneha
-      </div>
-    </>
-  )
+    return (
+       <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Login/>}/>
+            <Route path='/register' element={<Register/>}/>
+            <Route path='/nav' element={<Navbar/>}/>
+          </Routes>
+        </BrowserRouter>
+       </div>
+    )
 }
 
 export default App
